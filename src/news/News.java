b9f94d0071aceb6a7cac08e7b02519bba7d61f17
alexf07 <alexfloread98@gmail.com>
2020-12-08 18:25:34 +0200

    users & news

---
+++ src/news/News.java
@@ -0,0 +1,61 @@
+package news;
+
+import java.util.Date;
+
+public class news
+{
+    private final String title;
+    private final Date publishdate;
+    private final String category;
+    private final String author;
+    private String content;
+    private Date lastChangeDate;
+
+    public News(String category,String author,String title,String content)
+    {
+        this.title=title;
+        publishdate=new Date();
+        this.category=category;
+        this.author=author;
+        this.content=content;
+        lastChangeDate=new Date();
+    }
+
+    public getCategory()
+    {
+        return category;
+    }
+    private void updateLastChangeDate()
+    {
+        lastChangeDate=new Date();
+    }
+    public void changeContent(String content)
+    {
+        this.content = content;
+        updateLastChangeDate();
+    }
+    public String toString()
+    {
+        StringBuilder messageBuilder = new StringBuilder();
+		messageBuilder.append("[" + this.category + "]: ").append(this.title);
+		messageBuilder.append("\n");
+		messageBuilder.append("Written by: " + this.author);
+		messageBuilder.append("\n");
+		messageBuilder.append(this.content);
+
+		return messageBuilder.toString();
+    }
+
+    public boolean Equals(Object Object)
+    {
+        if(object instanceof News)
+        {
+            News newz = (News) object;
+            return newz.author==this.author&&newz.title=title;
+        }
+        return false;
+    }
+
+
+
+}
